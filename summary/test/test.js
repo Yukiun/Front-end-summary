da017e3b3cb458b0b81301752e0e9b11dedd5600
王亚星 <funnycoderstar@gmail.com>
2017-07-11 23:43:35 +0800

    feat: 输出一个数组里最大的3个数的积

--- summary/test/test.js
+++ summary/test/test.js
@@ -1,7 +1,34 @@
-var http = require('http');
-var counter = 0;
-var server = http.createServer(function(req, res) {
-    counter++;
-    res.write('I have been accessed' +counter + 'times.');
-    res.end();
-}).listen(8888);
+// var http = require('http');
+// var counter = 0;
+// var server = http.createServer(function(req, res) {
+//     counter++;
+//     res.write('I have been accessed' +counter + 'times.');
+//     res.end();
+// }).listen(8888);
+
+function foo(x, y, z) {
+    function func() {};
+    var func;
+    console.log(func);
+}
+foo(100);
+// function func() {}
+function foo(x, y, z) {
+    function func() {};
+    var func = 1;
+    console.log(func);
+}
+foo(100);
+// 1
+
+function fn(a) {
+    console.log(a);
+    var a = 2;
+    function a() {};
+    console.log(a);
+}
+fn(1);
+// function a() {}
+// 2
+a();
+//  ReferenceError: a is not defined
